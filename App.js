288a93be745c69721a628a4e050a8333a45871dc
chrockny <chrockny1007@gmail.com>
2021-05-02 20:31:59 -0500

    ARCHIVO ZIP

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MyDrawer from './routes/MyDrawer';
 import {NavigationContainer} from '@react-navigation/native'
+import {Provider} from 'react-redux';
+import store from './store/store';
 
 export default function App() {
   return (
+    <Provider store={store}>
     <NavigationContainer>
       <MyDrawer/>
     </NavigationContainer>
+    </Provider>
   );
 }
